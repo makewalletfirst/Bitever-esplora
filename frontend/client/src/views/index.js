b6cbf9d184fee5259a505d1289f526663617ed78
makewalletfirst <makewalletfirst@gmail.com>
2025-09-21 14:49:17 +0900

    Fix: add frontend as normal folder instead of submodule

---
+++ frontend/client/src/views/index.js
@@ -0,0 +1,21 @@
+export { dashBoard }       from './home'
+export { default as apiLanding } from './lander'
+export { recentBlocks }       from './blocks-all'
+export { recentTxs }          from './transactions-all'
+export { default as block }   from './block'
+export { default as addr }    from './addr'
+export { default as tx }      from './tx'
+export { default as pushtx }  from './pushtx'
+export { default as scan }    from './scan'
+export { default as mempool } from './mempool'
+export { default as loading } from './loading'
+export { error, notFound }    from './error'
+
+// Elements
+if (process.env.IS_ELEMENTS) {
+  exports.asset = require('./asset').default
+
+  if (process.env.ASSET_MAP_URL) {
+    exports.assetList = require('./asset-list').default
+  }
+}
